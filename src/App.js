e85746503ff6e36d18aa6b054bd7620fbbca0a6b
devadharshi <135523306+devadharshi@users.noreply.github.com>
2023-06-21 18:54:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './component/Calculator.js';
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
+
